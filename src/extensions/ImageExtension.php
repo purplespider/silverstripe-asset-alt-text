b6519f9b62197e482053a401bc6130fe5fb655bc
James Cocker <git@pswd.biz>
2021-07-09 15:26:26 +0100

    Removed unused use statements

--- src/extensions/ImageExtension.php
+++ src/extensions/ImageExtension.php
@@ -2,8 +2,6 @@
 
 namespace PurpleSpider\AssetAltText;
 
-use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataExtension;
 
 class ImageExtension extends DataExtension
