46100d9165df827199c4a86bfd0ad19567501778
James (DPC) <james.ellis@dpc.nsw.gov.au>
2022-08-18 17:52:58 +1000

    improve tests

--- src/extensions/ImageExtension.php
+++ src/extensions/ImageExtension.php
@@ -6,18 +6,18 @@ use SilverStripe\ORM\DataExtension;
 
 class ImageExtension extends DataExtension
 {
-    
+
     private static $db = [
         "AltText" => "Text",
     ];
-    
+
     /**
-     * Apply alt attribute if available, automatically
+     * Apply alt attribute if a non empty string, automatically
      * via ImageManipulation trait extension
      * @param array $attributes
      */
-    public function updateAttributes(array $attributes) {
-        if($this->owner->AltText !== '') {
+    public function updateAttributes(array &$attributes) {
+        if(!is_null($this->owner->AltText) && $this->owner->AltText !== '') {
             $attributes['alt'] = $this->owner->AltText;
         }
     }
